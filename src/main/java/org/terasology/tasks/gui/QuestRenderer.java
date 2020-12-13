0e2df88192b79d3e005bbcc1c34e648e89c47da2
Michael Pollind <mpollind@gmail.com>
2020-12-12 21:40:05 -0800

    feat(JOML): migrate joml primatives 1.9.26     PR: https://github.com/MovingBlocks/Terasology/pull/4320

--- src/main/java/org/terasology/tasks/gui/QuestRenderer.java
+++ src/main/java/org/terasology/tasks/gui/QuestRenderer.java
@@ -15,7 +15,7 @@
  */
 package org.terasology.tasks.gui;
 
-import org.joml.Rectanglei;
+import org.joml.primitives.Rectanglei;
 import org.joml.Vector2i;
 import org.terasology.math.geom.Rect2i;
 import org.terasology.nui.Canvas;
