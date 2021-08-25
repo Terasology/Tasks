9d5ff1731b8c0db2efca0e3326c0f84ecdcb66cf
Nail Khanipov <foxflameless@gmail.com>
2021-08-25 19:37:57 +0300

    fix(ecs-gestalt): Migrate Components to gestalt's Components. (#27)
    Ref: MovingBlocks/Terasology#4753
    
    Co-authored-by: Michael Pollind <mpollind@gmail.com>
    Co-authored-by: Tobias Nett <skaldarnar@googlemail.com>

--- src/main/java/org/terasology/tasks/components/QuestListComponent.java
+++ src/main/java/org/terasology/tasks/components/QuestListComponent.java
@@ -1,30 +1,23 @@
-/*
- * Copyright 2015 MovingBlocks
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *  http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// Copyright 2021 The Terasology Foundation
+// SPDX-License-Identifier: Apache-2.0
 
 package org.terasology.tasks.components;
+
+import org.terasology.gestalt.entitysystem.component.Component;
 
 import java.util.ArrayList;
 import java.util.List;
 
-import org.terasology.engine.entitySystem.Component;
-
 /**
  * Provides a list of available quests.
  */
-public final class QuestListComponent implements Component {
+public final class QuestListComponent implements Component<QuestListComponent> {
 
     public List<String> questItems = new ArrayList<>();
+
+    @Override
+    public void copyFrom(QuestListComponent other) {
+        this.questItems.clear();
+        this.questItems.addAll(other.questItems);
+    }
 }
